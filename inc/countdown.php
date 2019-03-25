fe01f17519f5ac315e5d4a73403c0be186478c02
RootRelien <root@aurelien-ThinkPad-T420>
2019-03-25 17:07:59 +0100

    countdown

--- inc/countdown.php
+++ inc/countdown.php
@@ -1,1 +1,7 @@
-<div id="countdown" data-wow-delay=".1s" data-date="Mai 11, 2019 09:00:00"></div>
+<div class="container">
+  <div class="row ">
+    <div class="col-md-12 col-sm-12 text-center align-items-center">
+      <div id="countdown" data-wow-delay=".1s" data-date="May 11, 2019 09:00:00"></div>
+    </div>
+  </div>
+</div>
--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 (function ($) {
-    
+
     // Init Wow
     wow = new WOW( {
         animateClass: 'animated',
         offset:       100
     });
     wow.init();
-    
+
     // Countdown
-    if ($('#countdown').length) { 
+    if ($('#countdown').length) {
         $('#countdown').countdown({
             render: function(data) {
                 if (data.years >= 1) {
@@ -17,13 +17,13 @@
                     var $days = data.days;
                 }
                 $(this.el).html(
-                    '<div class="day">' + this.leadingZeros($days) + ' <span>Days</span></div>'+
-                    '<div class="hour">' + this.leadingZeros(data.hours, 2) + ' <span>Hours</span></div>'+
-                    '<div class="min">' + this.leadingZeros(data.min, 2) + ' <span>Minutes</span></div>'+
-                    '<div class="sec">' + this.leadingZeros(data.sec, 2) + ' <span>Seconds</span></div>'
+                    '<p class="day">' + this.leadingZeros($days) + ' j : '
+                    + this.leadingZeros(data.hours, 2) + ' h : '+
+                    this.leadingZeros(data.min, 2) + ' m : '+
+                    this.leadingZeros(data.sec, 2) + ' s</p>'
                 );
             }
         });
     }
-    
+
 })(jQuery);
