f4dde35addec074a9214cc4a16fbe210294d1c4e
RootRelien <root@aurelien-ThinkPad-T420>
2019-03-28 12:18:01 +0100

    responsivity countdown + H

--- inc/countdown.php
+++ inc/countdown.php
@@ -1,14 +1,14 @@
 
 
-    <div class="row justify-content-center">
-      <div class="col-md-3">
+    <div class="row justify-content-center align-items-center flex-nowrap">
+      <div class=" col-sm-2 col-md-3">
 
       </div>
-      <div class="col-md-6 text-center">
+      <div class=" col-sm-8 col-md-6 text-center">
         <div id="countdown" data-wow-delay=".1s" data-date="May 11, 2019 09:00:00">
         </div>
       </div>
-      <div class="col-md-3 ">
+      <div class=" col-sm-2 col-md-3">
 
       </div>
     </div>
