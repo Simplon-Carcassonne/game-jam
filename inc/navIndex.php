450b411fb45c8d73ab7575d8356ab989579bc8b6
RootRelien <root@aurelien-ThinkPad-T420>
2019-03-28 15:02:52 +0100

    gamification nabar

--- inc/navIndex.php
+++ inc/navIndex.php
@@ -2,16 +2,14 @@
 
   <a href="#top"><i class="fas fa-arrow-up"></i></a>
   <!-- <li> <a href="#counter">countdown</a></li> -->
-  <a href="#moreInfos">> MoreInfos</a>
-  <a href="#whoWeAre">> whoWeAre</a>
-  <a href="#contact-info">> contact-info</a>
+  <a href="#moreInfos"><i class="fas fa-info-circle"></i></a>
+  <a href="#whoWeAre"><i class="fas fa-question-circle"></i></a>
+  <a href="#contact-info"><i class="fas fa-address-card"></i></a>
   <!-- <li> <a href="#socialmedia"> socialmedia</a></li> -->
-  <a href="#mapid">> Map</a>
+  <a href="#mapid"><i class="fas fa-map-marked-alt"></i></a>
   <a href="#foot"><i class="fas fa-arrow-down"></i></a>
 
-
-<br>
-      <a href="https://www.facebook.com/Game-Jam-Carcassonne-545868659270573/"target="_blank"><i class="fab fa-facebook"></i></a>
-
+  <br>
+  <a href="https://www.facebook.com/Game-Jam-Carcassonne-545868659270573/"target="_blank"><i class="fab fa-facebook"></i></a>
 
 </div>
