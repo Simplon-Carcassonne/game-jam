f033c4b3db5645614f1e1e596f75d60d5ab5e47d
RootRelien <root@aurelien-ThinkPad-T420>
2019-03-29 12:24:19 +0100

    section infos pratiques

--- inc/whereEatSleep.php
+++ inc/whereEatSleep.php
@@ -1,15 +1,54 @@
 <section class="container">
-    <div class="row">
-        <div class="col-lg-12  about-title">
-            <h2>infos pratiques</h2>
-            <hr id="whereEat">
-            <div class="center" >
-              <h3>Hôtels</h3>
-              <h3>Restauration</h3>
-              <h3>Parking</h3>
+  <div class="row">
+    <div class="col-lg-12  about-title">
+      <h2>infos pratiques</h2>
+      <hr id="whereEat">
+      <div class="center" >
+        <h3>Hôtels</h3>
 
-            </div>
-        </div>
+        <ul>
+          <li> <p> <a href="https://www.hifrance.org/auberge-de-jeunesse/carcassonne.html" target="_blank">Auberge de Jeunesse</a> , 8 Rue Raymond Roger Trencavel, 11000 Carcassonne - 04 68 25 23 16</p> </li>
+          <li> <p> <a href="http://hotel-carcassonne-11.com" target="_blank"> Hotel Central</a> , 27 Boulevard Jean Jaurès, 11000 Carcassonne - 04 68 25 03 84</p> </li>
+          <li> <p> <a href="https://www.accorhotels.com/fr/hotel-1371-ibis-carcassonne-centre-la-cite/index.shtm" target="_blank">Hotel IBIS</a> , 5 Square Gambetta, 11000 Carcassonne - 04 68 72 37 37</p> </li>
+          <li> <p> <a href="https://www.accorhotels.com/fr/hotel-3689-ibis-budget-carcassonne-la-cite/index.shtml" target="_blank">Hotel Ibis Budget Carcassonne La cité</a> , 211 Avenue du Général Leclerc, 11000 Carcassonne - 0 892 68 09 14</p> </li>
+          <li> <p> <a href="https://hotel-carcassonne-bosquet.brithotel.fr" target="_blank">Brit Hotel Le Bosquet</a> , 8 rue des Coteaux de Pech Mary, 11000 Carcassonne - 04 68 72 78 78 </p> </li>
+          <li> <p> <a href="https://www.hotel-audotel.com" target="_blank">Audotel</a> , 113 Route nationale, 11000 Carcassonne - 04 68 79 83 30 </p> </li>
+        </ul>
+
+        <h3>Restauration</h3>
+
+        <h4>Le livreur</h4>
+        <ul>
+          <li><a href="https://sacrefernand.com/" target="_blank">Sacré Fernand</a> : La boussole des gourmets </li>
+        </ul>
+
+
+        <h4>Snack</h4>
+        <ul>
+          <li> <p> <a href="https://www.facebook.com/takeoffpizzafranceofficiel/">Take Off Pizza</a> : livraison ou 145 av. Franklin Roosevelt, 11000 Carcassonne - 04 68 47 53 48</p> </li>
+          <li> <p> <a href="https://www.facebook.com/Meery-Cake-135627053291530/"> Meery Cakes</a> : 13 Rue Barbès, 11000 Carcassonne - 09 83 60 56 02</p> </li>
+          <li> <p> <a href="https://www.facebook.com/Kebab-Le-Casablanca-500370973423614/"> Le Casablanca</a> : 19 Boulevard Jean Jaurès, 11000 Carcassonne - 09 52 50 12 91</p> </li>
+          <li> <p> <a href="https://www.facebook.com/Tacos-Loasis-Carcassonne-815653538484119/">L’Oasis</a> :  22 Rue de la république, 11000 Carcassonne - 04 34 58 93 22</p> </li>
+          <li> <p> <a href="https://www.facebook.com/pages/category/Restaurant/La-Ptite-TABLE-260654850949913/"> La P'tite Table</a> : 24 Route Minervoise, 11000 Carcassonne - 04 34 58 93 22</p> </li>
+        </ul>
+
+        <h4>Boulangerie</h4>
+        <ul>
+        <li> <p>Boulangerie Pâtisserie Papineau, 57 rue de Verdun, 11000 Carcassonne - 04 68 47 67 51</p> </li>
+        <li>  <p>Briocherie Arpin, 4 Place Carnot, 11000 Carcassonne - 04 68 25 12 59</p> </li>
+        <li> <p>Boulangerie des Halles, 61 Rue Aimé Ramond, 11000 Carcassonne - 04 68 25 06 17</p> </li>
+        </ul>
+
+        <h4>Supermarché</h4>
+        <ul>
+        <li> <p>Carrefour Market, 17 Place Carnot, 11000 Carcassonne</p> </li>
+        <li> <p>Monoprix, 17 Rue Georges Clemenceau, 11000 Carcassonne</p> </li>
+        </ul>
+
+        <h3>Parking</h3>
+
+      </div>
     </div>
+  </div>
 </section>
 <br><br>
