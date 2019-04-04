85e520c8d8ac2dd43fe28430cf797f6607c0ffb8
alto971 <mafabrikjeux@gmail.com>
2019-04-04 21:45:23 +0200

    add wording for teams with no tune

--- inc/whereEatSleep.php
+++ inc/whereEatSleep.php
@@ -3,20 +3,43 @@
     <div class="col-lg-12  about-title">
       <h2>infos pratiques</h2>
       <hr>
-      <div class="center" >
-        <p>Nous invitons à réserver à l'avance votre hôtel, Carcassonne en période estivale reçoit bon nombre de touristes, vous trouverez une liste non exhaustive d'hôtels, snacks & commerces utiles</p>
+      <div class="center">
+        <p>Nous invitons à réserver à l'avance votre hôtel,
+          Carcassonne en période estivale reçoit bon nombre de touristes,
+          vous trouverez une liste non exhaustive d'hôtels, snacks & commerces utiles<br><br>
+
+          -- Facilités pour les équipes --<br>
+          Si votre budget est limité, il est possible
+          de dormir dans un gymnase, à 10 min à pied de la salle. 
+          (prévoir dans ce cas votre matériel de couchage)<br>
+          De plus le staff d'organisation de la Game Jam s'occupe de travailler
+          avec les partenaires de restauration, pour vous offrir des menus à des prix concurentiels
+        </p>
+
       </div>
       <div class="col-lg-12  infoModal">
         <button id="hotelBtn" type="button" name="button"> Hôtels</button>
         <div class="" id="hotelModal">
           <h4>Hôtels</h4>
           <ul>
-            <li> <p> <a href="https://www.hifrance.org/auberge-de-jeunesse/carcassonne.html" target="_blank">Auberge de Jeunesse</a> , 8 Rue Raymond Roger Trencavel, 11000 Carcassonne - 04 68 25 23 16</p> </li>
-            <li> <p> <a href="http://hotel-carcassonne-11.com" target="_blank"> Hotel Central</a> , 27 Boulevard Jean Jaurès, 11000 Carcassonne - 04 68 25 03 84</p> </li>
-            <li> <p> <a href="https://www.accorhotels.com/fr/hotel-1371-ibis-carcassonne-centre-la-cite/index.shtml" target="_blank">Hotel IBIS</a> , 5 Square Gambetta, 11000 Carcassonne - 04 68 72 37 37</p> </li>
-            <li> <p> <a href="https://www.accorhotels.com/fr/hotel-3689-ibis-budget-carcassonne-la-cite/index.shtml" target="_blank">Hotel Ibis Budget Carcassonne La cité</a> , 211 Avenue du Général Leclerc, 11000 Carcassonne - 0 892 68 09 14</p> </li>
-            <li> <p> <a href="https://hotel-carcassonne-bosquet.brithotel.fr" target="_blank">Brit Hotel Le Bosquet</a> , 8 rue des Coteaux de Pech Mary, 11000 Carcassonne - 04 68 72 78 78 </p> </li>
-            <li> <p> <a href="https://www.hotel-audotel.com" target="_blank">Audotel</a> , 113 Route nationale, 11000 Carcassonne - 04 68 79 83 30 </p> </li>
+            <li>
+              <p> <a href="https://www.hifrance.org/auberge-de-jeunesse/carcassonne.html" target="_blank">Auberge de Jeunesse</a> , 8 Rue Raymond Roger Trencavel, 11000 Carcassonne - 04 68 25 23 16</p>
+            </li>
+            <li>
+              <p> <a href="http://hotel-carcassonne-11.com" target="_blank"> Hotel Central</a> , 27 Boulevard Jean Jaurès, 11000 Carcassonne - 04 68 25 03 84</p>
+            </li>
+            <li>
+              <p> <a href="https://www.accorhotels.com/fr/hotel-1371-ibis-carcassonne-centre-la-cite/index.shtml" target="_blank">Hotel IBIS</a> , 5 Square Gambetta, 11000 Carcassonne - 04 68 72 37 37</p>
+            </li>
+            <li>
+              <p> <a href="https://www.accorhotels.com/fr/hotel-3689-ibis-budget-carcassonne-la-cite/index.shtml" target="_blank">Hotel Ibis Budget Carcassonne La cité</a> , 211 Avenue du Général Leclerc, 11000 Carcassonne - 0 892 68 09 14</p>
+            </li>
+            <li>
+              <p> <a href="https://hotel-carcassonne-bosquet.brithotel.fr" target="_blank">Brit Hotel Le Bosquet</a> , 8 rue des Coteaux de Pech Mary, 11000 Carcassonne - 04 68 72 78 78 </p>
+            </li>
+            <li>
+              <p> <a href="https://www.hotel-audotel.com" target="_blank">Audotel</a> , 113 Route nationale, 11000 Carcassonne - 04 68 79 83 30 </p>
+            </li>
           </ul>
           <button id="closeHotel" type="button" name="button">X</button>
         </div>
@@ -28,17 +51,33 @@
           </ul>
           <h4>Snack</h4>
           <ul>
-            <li> <p> <a href="https://www.facebook.com/takeoffpizzafranceofficiel/">Take Off Pizza</a> : livraison ou 145 av. Franklin Roosevelt, 11000 Carcassonne - 04 68 47 53 48</p> </li>
-            <li> <p> <a href="https://www.facebook.com/Meery-Cake-135627053291530/"> Meery Cakes</a> : 13 Rue Barbès, 11000 Carcassonne - 09 83 60 56 02</p> </li>
-            <li> <p> <a href="https://www.facebook.com/Kebab-Le-Casablanca-500370973423614/"> Le Casablanca</a> : 19 Boulevard Jean Jaurès, 11000 Carcassonne - 09 52 50 12 91</p> </li>
-            <li> <p> <a href="https://www.facebook.com/Tacos-Loasis-Carcassonne-815653538484119/">L’Oasis</a> :  22 Rue de la république, 11000 Carcassonne - 04 34 58 93 22</p> </li>
-            <li> <p> <a href="https://www.facebook.com/pages/category/Restaurant/La-Ptite-TABLE-260654850949913/"> La P'tite Table</a> : 24 Route Minervoise, 11000 Carcassonne - 04 34 58 93 22</p> </li>
+            <li>
+              <p> <a href="https://www.facebook.com/takeoffpizzafranceofficiel/">Take Off Pizza</a> : livraison ou 145 av. Franklin Roosevelt, 11000 Carcassonne - 04 68 47 53 48</p>
+            </li>
+            <li>
+              <p> <a href="https://www.facebook.com/Meery-Cake-135627053291530/"> Meery Cakes</a> : 13 Rue Barbès, 11000 Carcassonne - 09 83 60 56 02</p>
+            </li>
+            <li>
+              <p> <a href="https://www.facebook.com/Kebab-Le-Casablanca-500370973423614/"> Le Casablanca</a> : 19 Boulevard Jean Jaurès, 11000 Carcassonne - 09 52 50 12 91</p>
+            </li>
+            <li>
+              <p> <a href="https://www.facebook.com/Tacos-Loasis-Carcassonne-815653538484119/">L’Oasis</a> : 22 Rue de la république, 11000 Carcassonne - 04 34 58 93 22</p>
+            </li>
+            <li>
+              <p> <a href="https://www.facebook.com/pages/category/Restaurant/La-Ptite-TABLE-260654850949913/"> La P'tite Table</a> : 24 Route Minervoise, 11000 Carcassonne - 04 34 58 93 22</p>
+            </li>
           </ul>
           <h4>Boulangerie</h4>
           <ul>
-            <li> <p>Boulangerie Pâtisserie Papineau, 57 rue de Verdun, 11000 Carcassonne - 04 68 47 67 51</p> </li>
-            <li>  <p>Briocherie Arpin, 4 Place Carnot, 11000 Carcassonne - 04 68 25 12 59</p> </li>
-            <li> <p>Boulangerie des Halles, 61 Rue Aimé Ramond, 11000 Carcassonne - 04 68 25 06 17</p> </li>
+            <li>
+              <p>Boulangerie Pâtisserie Papineau, 57 rue de Verdun, 11000 Carcassonne - 04 68 47 67 51</p>
+            </li>
+            <li>
+              <p>Briocherie Arpin, 4 Place Carnot, 11000 Carcassonne - 04 68 25 12 59</p>
+            </li>
+            <li>
+              <p>Boulangerie des Halles, 61 Rue Aimé Ramond, 11000 Carcassonne - 04 68 25 06 17</p>
+            </li>
           </ul>
           <button id="closeResto" type="button" name="button">X</button>
         </div>
@@ -46,13 +85,21 @@
         <div class="" id="diversModal">
           <h4>Supermarché</h4>
           <ul>
-            <li> <p>Carrefour Market, 17 Place Carnot, 11000 Carcassonne</p> </li>
-            <li> <p>Monoprix, 17 Rue Georges Clemenceau, 11000 Carcassonne</p> </li>
+            <li>
+              <p>Carrefour Market, 17 Place Carnot, 11000 Carcassonne</p>
+            </li>
+            <li>
+              <p>Monoprix, 17 Rue Georges Clemenceau, 11000 Carcassonne</p>
+            </li>
           </ul>
           <h4>pharmacie</h4>
           <ul>
-            <li> <p>Pharmacie Moutou : 14 Rue Chartrand, 11000 Carcassonne - 04 68 25 06 87</p> </li>
-            <li> <p>Pharmacie Huc : 26 Rue de Verdun, 11000 Carcassonne - 04 68 25 04 51</p> </li>
+            <li>
+              <p>Pharmacie Moutou : 14 Rue Chartrand, 11000 Carcassonne - 04 68 25 06 87</p>
+            </li>
+            <li>
+              <p>Pharmacie Huc : 26 Rue de Verdun, 11000 Carcassonne - 04 68 25 04 51</p>
+            </li>
           </ul>
           <button id="closeDivers" type="button" name="button">X</button>
         </div>
